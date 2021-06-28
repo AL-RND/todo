c6dd8eca35ecc6d111d3c4ae7166407290ffd644
aleks <aleks94@me.com>
2021-06-28 21:38:39 +0200

    Initial commit

---
+++ src/main/java/com/example/todo/model/TodoItem.java
@@ -0,0 +1,74 @@
+package com.example.todo.model;
+
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class TodoItem {
+
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long itemId;
+
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID listId;
+
+    @NotEmpty(message="* Enter Task Name")
+    private String taskName;
+
+    public TodoItem() {}
+
+    public TodoItem(UUID listId, String taskName, Date createdAt) {
+        this.listId = listId;
+        this.taskName = taskName;
+        this.createdAt = createdAt;
+    }
+
+    private Boolean isDone = false; // Default value
+
+    private Date createdAt = new Date();
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public UUID getListId() {
+        return listId;
+    }
+
+    public void setListId(UUID listId) {
+        this.listId = listId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public Boolean getDone() {
+        return isDone;
+    }
+
+    public void setDone(Boolean done) {
+        isDone = done;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
+
